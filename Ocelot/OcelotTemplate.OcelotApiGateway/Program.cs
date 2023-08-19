08ea83eb018073905ca1362d3bbc8fa6910b30d5
Pc <Pc@DESKTOP-PRAMDG2>
2023-08-19 16:17:48 +0330

    Starting The Ocelot Configuration

--- Ocelot/OcelotTemplate.OcelotApiGateway/Program.cs
+++ Ocelot/OcelotTemplate.OcelotApiGateway/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+var environmentEnvironmentName = builder.Environment.EnvironmentName;
+builder.Configuration.AddJsonFile($"ocelot{environmentEnvironmentName}.json", false, true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
